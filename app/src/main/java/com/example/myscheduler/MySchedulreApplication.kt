650131ae61391cd29a736170c31c1a524f4b0321
yshibasaki <shibasaki.yutaro@gb-sense.com>
2020-03-03 18:00:53 +0900

    DBテスト格闘

--- app/src/main/java/com/example/myscheduler/MySchedulreApplication.kt
+++ app/src/main/java/com/example/myscheduler/MySchedulreApplication.kt
@@ -3,7 +3,7 @@ package com.example.myscheduler
 import android.app.Application
 import io.realm.Realm
 
-class MySchedulreApplication : Application() {
+class MySchedulerApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Realm.init(this)
